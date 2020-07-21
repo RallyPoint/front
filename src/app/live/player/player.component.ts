8c294c9bf3e64cfc3d2d991acf51e24250958434
franck <franck.rst@gmail.com>
2020-07-21 16:49:53 +0200

    WIP

--- src/app/live/player/player.component.ts
+++ src/app/live/player/player.component.ts
@@ -1,44 +1,51 @@
-import {ElementRef, Component, OnInit, ViewChild} from '@angular/core';
-import * as p2pml from 'p2p-media-loader-shaka';
-import * as shaka from 'shaka-player';
-import * as muxJs from 'mux.js';
+import {ElementRef, Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 import {EventEmitter} from 'events';
+declare var p2pml: any;
+declare var shaka: any;
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements AfterViewInit {
 
-  @ViewChild('playerEl') playerEl:ElementRef;
+  @ViewChild('playerEl', {read: ElementRef}) playerEl: ElementRef;
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  onBytesDownloaded(method, size) {
+    console.log(method);
+  }
+  onBytesUploaded(method, size) {
+    console.log(method);
+  }
+  ngAfterViewInit(): void {
     shaka.polyfill.installAll();
-    if (shaka.Player.isBrowserSupported() && p2pml.Engine.isSupported()) {
-      const engine = new p2pml.Engine();
+    if (shaka.Player.isBrowserSupported() && p2pml.shaka.Engine.isSupported()) {
+      const engine = new p2pml.shaka.Engine({
+        loader : {
+          trackerAnnounce : ['wss://tracker.openwebtorrent.com']
+        }
+      });
+      engine.on(p2pml.core.Events.PieceBytesDownloaded, this.onBytesDownloaded.bind(this));
 
-      var player = new shaka.Player(this.playerEl.nativeElement);
-      player.addEventListener("error",(error) => { console.error("Error code", error.detail.code, "object", error.detail); });
+      engine.on(p2pml.core.Events.PieceBytesUploaded, this.onBytesUploaded.bind(this));
+      const player = new shaka.Player(this.playerEl.nativeElement);
+
+      player.addEventListener('error', (error) => { console.error('Error code', error.detail.code, 'object', error.detail); });
+
+      (window as any).shaka = shaka;
 
       engine.initShakaPlayer(player);
 
-      player.load("https://akamai-axtest.akamaized.net/routes/lapd-v1-acceptance/www_c4/Manifest.mpd").catch(
-        (error) => { console.error("Error code", error.code, "object", error); }
+      player.load('https://stats.rallypoint.tech/live/zHqELc7G0rIcogKueApdDNeShT3uwUQR.m3u8').catch(
+        (error) => { console.error('Error code', error.code, 'object', error); }
       );
     } else {
-      document.write("Not supported :(");
+      document.write('Not supported :(');
     }
   }
 
-}
-interface Tata {
-  on(eventName: string, listener: (...params: any[]) => void): this;
-}
-class Toto extends EventEmitter implements Tata{
-  lol(){
-    console.log('aze');
-  }
 }
