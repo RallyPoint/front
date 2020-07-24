91ffef7d1c093909a1886393c0c767f9f5232861
franck <franck.rst@gmail.com>
2020-07-24 14:22:52 +0200

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.rallypoint.tech',
-  liveUrl: 'https://stat.rallypoint.tech'
+  liveUrl: 'https://stats.rallypoint.tech'
 };
