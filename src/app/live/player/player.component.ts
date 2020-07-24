9420867e9b98be41adeb25c0751b2132f19eeef2
franck <franck.rst@gmail.com>
2020-07-24 15:49:58 +0200

    WIP

--- src/app/live/player/player.component.ts
+++ src/app/live/player/player.component.ts
@@ -1,7 +1,7 @@
 import {ElementRef, Component, OnInit, ViewChild, AfterViewInit, Input} from '@angular/core';
 import {environment} from '../../../environments/environment';
 declare var p2pml: any;
-declare var shaka: any;
+declare var Clappr: any;
 
 @Component({
   selector: 'app-player',
@@ -25,31 +25,60 @@ export class PlayerComponent implements AfterViewInit {
     console.log(method);
   }
   ngAfterViewInit(): void {
-    shaka.polyfill.installAll();
-    if (shaka.Player.isBrowserSupported() && p2pml.shaka.Engine.isSupported()) {
-      const engine = new p2pml.shaka.Engine({
-        loader : {
-          trackerAnnounce : ['wss://tracker.openwebtorrent.com']
+    if (p2pml.hlsjs.Engine.isSupported()) {
+      const engine = new p2pml.hlsjs.Engine({
+        loader: {
+          trackerAnnounce: ['wss://tracker.openwebtorrent.com']
         }
       });
       engine.on(p2pml.core.Events.PieceBytesDownloaded, this.onBytesDownloaded.bind(this));
 
       engine.on(p2pml.core.Events.PieceBytesUploaded, this.onBytesUploaded.bind(this));
-      const player = new shaka.Player(this.playerEl.nativeElement);
-      player.addEventListener('error', (error) => { console.error('Error code', error.detail.code, 'object', error.detail); });
 
-      (window as any).shaka = shaka;
 
-      engine.initShakaPlayer(player);
+      const setup = {
+        parentId: '#video',
+        plugins: [],
+        source: 'https://stats.rallypoint.tech/live/' + this.channel + '.m3u8',
+        width: '100%',
+        height: '100%',
+        muted: true,
+        mute: true,
+        autoPlay: true,
+        playback: {
+          playInline: true,
+          liveSyncDurationCount: 7,
+          loader: engine.createLoaderClass()
+        }
+      };
+      const outer = document.createElement('div');
+      outer.className = 'embed-responsive embed-responsive-16by9';
+      const video = document.createElement('div');
+      video.id = 'video';
+      video.className = 'embed-responsive-item';
+      outer.appendChild(video);
+      this.playerEl.nativeElement.appendChild(outer);
 
-      player.load(environment.liveUrl + '/live/' + this.channel + '.m3u8').then(()=>{
-        console.log("======>");
-        this.playerEl.nativeElement.play();
-      }).catch(
-        (error) => { console.error('Error code', error.code, 'object', error); }
-      );
-    } else {
-      document.write('Not supported :(');
+      const player = new Clappr.Player(setup);
+      /*
+            const player = new shaka.Player(this.playerEl.nativeElement);
+            player.addEventListener('error', (error) => { console.error('Error code', error.detail.code, 'object', error.detail); });
+
+            (window as any).shaka = shaka;
+
+            engine.initShakaPlayer(player);
+
+            player.load('https://stats.rallypoint.tech/live/' + this.channel + '.m3u8').then(() => {
+              console.log('======>');
+              this.playerEl.nativeElement.play();
+            }).catch(
+              (error) => { console.error('Error code', error.code, 'object', error); }
+            );
+          } else {
+            document.write('Not supported :(');
+          }
+
+       */
     }
   }
 
