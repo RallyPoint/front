4c0f6a63c1c0e720f0dc5e353ea1bc43b2114b7a
franck <franck.rst@gmail.com>
2020-07-31 22:13:08 +0200

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.rallypoint.tech',
-  liveUrl: ['https://01.live.rallypoint.tech', 'https://02.live.rallypoint.tech'],
+  liveUrl: ['https://live01.rallypoint.tech', 'https://live02.rallypoint.tech'],
   chatUrl: 'https://chat.rallypoint.tech',
-  vodUrl: ['https://01.vod.rallypoint.tech', 'https://02.vod.rallypoint.tech']
+  vodUrl: ['https://vod01.rallypoint.tech', 'https://vod02.rallypoint.tech']
 };
