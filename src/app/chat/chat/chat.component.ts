56344de4fda30b30a20a62c40ab4ec03feceae21
franck <franck.rst@gmail.com>
2020-08-28 21:51:34 +0200

    WIP

--- src/app/chat/chat/chat.component.ts
+++ src/app/chat/chat/chat.component.ts
@@ -26,7 +26,6 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public io: Socket;
   public messages: {uuid: string, by: string, pseudo: string, txt: string, voteCount: number, votes: {[index: string]: number}, voted: number, gist?: string}[] = [
-    {pseudo: 'Maxence', by: 'azeaze', uuid: 'azeaze', txt: '@lesang2tmort parcequ\'il fais sont metier de streamer a perfection comme', voteCount: 0, votes: {}, voted: 0},
   ];
   public message: string;
   public autoScroll: boolean = true;
